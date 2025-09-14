470b475eab6ff2718bd43be5fc57f3e8bac01ac4
Eric Rhodes <e_rhodes@live.com>
2025-09-13 23:01:43 -0400

    value saving and button clicking now work

--- module/sheets/actor-sheet.js
+++ module/sheets/actor-sheet.js
@@ -15,17 +15,41 @@ export class FalloutActorSheet extends foundry.appv1.sheets.ActorSheet {
 	}
 
 	/** @override */
-	getData() {
+	async getData() {
 		const context = super.getData();
 
-		const actorData = context.data.system;
+		console.log(context);
 
-		console.log("actor data " + actorData);
+		// // Just for testing: add a laser property to the context
+		// context.laser = 12;
+
+	    // // Use a safe clone of the actor data for further operations.
+    	const actorData = this.document.toObject(false);
+
+
+	    // // Add the actor's data to context.data for easier access, as well as flags.
+	    context.system = actorData.system;
+	    // context.flags = actorData.flags;
 
 		return context;
 	}
 
 	get template() {
 		return 'systems/fallout/templates/actor-sheet.html';
+	}
+
+	/** @override */
+	activateListeners(html) {
+		super.activateListeners(html);
+
+		html.on('click', '.deal-damage', this._onDealDamage.bind(this));
+
+		// html.on('click', '.deal-damage', (ev) => {
+		// 	console.log("Deal damage button clicked");
+		// })
+	}
+
+	_onDealDamage(event) {
+		console.log("Deal damage button clicked");
 	}
 }
